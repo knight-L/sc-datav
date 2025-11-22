0b201a6739641d098f86db1fa4352287cbd59fb8
knight <941122922@qq.com>
2025-11-22 12:46:02 +0800

    feat: 增加路由功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import SCDataV from "./pages/SCDataV";
+import { Route, Routes } from "react-router";
 
 function App() {
-  return <SCDataV />;
+  return (
+    <Routes>
+      <Route path="/" element={<SCDataV />} />
+    </Routes>
+  );
 }
 
 export default App;
